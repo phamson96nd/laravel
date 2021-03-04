b108ecb1a55b1ff4b04712d653af66e4589fec00
son.pham <son.pham@kiaisoft.com>
2021-03-04 16:40:40 +0700

    setup login logout

--- LaravelB/routes/api.php
+++ LaravelB/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'User\AuthController@login');
+    Route::post('logout', 'User\AuthController@logout');
+    Route::post('refresh', 'User\AuthController@refresh');
+    Route::get('me', 'User\AuthController@me');
+
 });
